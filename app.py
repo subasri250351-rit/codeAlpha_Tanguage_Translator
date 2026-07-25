35726d9793c2f4fcd6171c846eaf461d34786cde
subasri250351-rit <subasri.250351@aids.ritchennai.edu.in>
2026-07-25 13:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from deep_translator import GoogleTranslator
+from googletrans import Translator
 
 st.title("Language Translation Tool")
 
@@ -9,6 +9,6 @@ source = st.text_input("Source language code (example: en)")
 target = st.text_input("Target language code (example: ta)")
 
 if st.button("Translate"):
-    if text:
-        result = GoogleTranslator(source=source, target=target).translate(text)
-        st.success(result)
+    translator = Translator()
+    result = translator.translate(text, src=source, dest=target)
+    st.success(result.text)
